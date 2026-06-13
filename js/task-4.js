2a2c27498aa2cf7b1a5e4c819f471d34831f975c
Andrii_lishchynskyi <lav95g@gmail.com>
2026-06-12 22:55:35 -0400

    update

--- js/task-4.js
+++ js/task-4.js
@@ -4,11 +4,5 @@ const getTotalBalanceByGender = (users, gender) => {
     .reduce((total, user) => total + user.balance, 0);
 };
 
-const getTotalBalanceByGender = (users, gender) => {
-  return users
-    .filter(user => user.gender === gender)
-    .reduce((total, user) => total + user.balance, 0);
-};
-
 console.log(getTotalBalanceByGender(users, "male"));
 console.log(getTotalBalanceByGender(users, "female"));
